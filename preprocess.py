2e1242ca80ebc0f0882965f01ce8b12b58776245
madcpt <zihan.xu@sjtu.edu.cn>
2019-12-19 14:47:16 +0800

    FIX bugs in SR module

--- preprocess.py
+++ preprocess.py
@@ -8,17 +8,10 @@ from torch.utils.data import Dataset, DataLoader, random_split, Subset
 
 
 class dataset(Dataset):
-    def __init__(self):
+    def __init__(self, normalize=True):
         self.data_file_path = 'A1Benchmark'
         self.sliding_window_size = 64
-        self.values, self.labels = self.load_data()
-
-    def amplify(self, X, Y, rate):
-        if random() < rate:
-            index = randrange(0, self.sliding_window_size)
-            X[index] += (sum(X[index + 1:]) + np.mean(X)) * (1 + np.var(X)) * np.random.normal(0, 1)
-            Y = True
-        return X, Y
+        self.values, self.labels = self.load_data(normalize=normalize)
 
     @staticmethod
     def slide_window(values, labels, window_size):
@@ -31,18 +24,19 @@ class dataset(Dataset):
             Ys.append(Y)
         return Xs, Ys
 
-    def load_data(self):
+    def load_data(self, normalize=True):
         all_data = [[], []]
         for i in range(1, 68):
             pathname = os.path.join(self.data_file_path, "real_" + str(i) + ".csv")
             data = pd.read_csv(pathname)
             values, labels = data['value'].tolist(), data['is_anomaly'].tolist()
             values_max, values_min = max(values), min(values)
-            values = [(value - values_min) / (values_max - values_min) for value in values]
+            if normalize:
+                values = [(value - values_min) / (values_max - values_min) for value in values]
             Xs, Ys = self.slide_window(values, labels, self.sliding_window_size)
             all_data[0] += Xs
             all_data[1] += Ys
-        return torch.tensor(all_data[0]), torch.tensor(all_data[1]).long()
+        return torch.tensor(all_data[0]).float(), torch.tensor(all_data[1]).long()
 
     def __getitem__(self, index):
         return self.values[index], self.labels[index]
@@ -51,30 +45,40 @@ class dataset(Dataset):
         return self.labels.size(0)
 
 
+# noinspection PyMissingConstructor
 class Amplify(dataset):
-    def __init__(self, train, rate=0.4):
-        # super().__init__()
-        self.values = train[0]
+    def __init__(self, train, rate=0.4, use_sr=False):
+        self.values = train[0].float()
         self.labels = train[1]
-        for i in range(train[0].size(0)):
+        for i in range(len(self)):
             X = train[0][i].numpy()
             if random() < rate:
                 index = randrange(0, len(X))
                 self.values[i][index] += (sum(X[index + 1:]) + np.mean(X)) * (1 + np.var(X)) * np.random.normal(0, 1)
                 self.labels[i] = True
+        if use_sr:
+            print('Using SR, should get yourself a cup of coffee.')
+            from SR.silency import Silency
+            amp_window_size = 4  # less than period
+            series_window_size = 4  # (maybe) as same as period
+            score_window_size = 16  # a number enough larger than period
+            spec = Silency(amp_window_size, series_window_size, score_window_size)
+            for i in range(len(self)):
+                self.values[i] = torch.from_numpy(spec.generate_anomaly_score(self.values[i].numpy())).float()
 
 
-def get_dataloader(batch_size=64):
-    data = dataset()
-    train_size = int(len(data) * 0.9)
+def get_dataloader(batch_size=64, rate=0.4, split=0.9, use_sr=False, normalize=True):
+    data = dataset(normalize)
+    train_size = int(len(data) * split)
     test_size = len(data) - train_size
     train_set, test_set = random_split(data, [train_size, test_size])
-    train_set = Amplify(train_set.dataset[np.array(train_set.indices)], 0.45)
+    test_set = Amplify(test_set.dataset[np.array(test_set.indices)], rate=0, use_sr=use_sr)
+    train_set = Amplify(train_set.dataset[np.array(train_set.indices)], rate=rate, use_sr=use_sr)
 
     print("Train Size: ", len(train_set), " Negative Rate: ",
           1 - sum(train_set.labels).item() / len(train_set))
     print("Test Size: ", len(test_set), " Negative Rate: ",
-          1 - sum(test_set.dataset.labels[np.array(test_set.indices)]).item() / len(test_set))
+          1 - sum(test_set.labels).item() / len(test_set))
 
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=4)
     test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True, num_workers=4)
@@ -82,7 +86,7 @@ def get_dataloader(batch_size=64):
 
 
 if __name__ == '__main__':
-    train, test = get_dataloader(64)
+    train, test = get_dataloader(batch_size=512, rate=0.4, split=0.9, use_sr=True, normalize=True)
     for data in train:
         print(data[0].shape)  # [batch, 60]
         print(data[1].shape)  # [batch]
